eda3c351d3d3b461772cc9ad54b78f50a7049dd1
Alwyn974 <alwyn974@gmail.com>
2024-07-16 17:39:59 +0400

    chore: Refactor add_member script to prompt for Epitech email
    This commit refactors the `add_member.sh` script to prompt the user for the Epitech email of the user to be added to the organization. This change improves the user experience by making the script more interactive.

--- scripts/add_member/add_member.sh
+++ scripts/add_member/add_member.sh
@@ -1,35 +1,155 @@
 #!/usr/bin/env bash
 
+# Set bash to exit
+set -e
+
+##############################
+#                            #
+#          VARIABLES         #
+#                            #
+##############################
+
+epitech_email="$EPITECH_EMAIL"
+
+jsreport_username="$JSREPORT_USERNAME"
+jsreport_password="$JSREPORT_PASSWORD"
+
+youtrack_token="$YOUTRACK_TOKEN"
+mailcow_token="$MAILCOW_TOKEN"
+infisical_token="$INFISICAL_TOKEN"
+figma_token="$FIGMA_TOKEN"
+gitea_token="$GITEA_TOKEN"
+lucidchart_token="$LUCIDCHART_TOKEN"
+coolify_token="$COOLIFY_TOKEN"
+
+username=$(echo $epitech_email | cut -d'@' -f1) # Username for pld, youtrack
+firstname=$(echo $epitech_email | cut -d'.' -f1) # Firstname
+firstname=${firstname^} # Capitalize the first letter
+lastname=$(echo $epitech_email | cut -d'.' -f2) # Lastname
+lastname=${lastname^} # Capitalize the first letter
+
 # Get epitech epitech email
 
-echo -n "Enter the epitech email of the user you want to add to the organization: "
-read email
+if [ -z "$epitech_email" ]; then
+    echo -n "Enter the epitech email of the user you want to add to the organization: "
+    read epitech_email
+fi
 
-# Get youtrack token
+##############################
+#                            #
+#          FUNCTIONS         #
+#                            #
+##############################
 
-# Get mailcow token
 
-# Get infisical token
+function add_user_to_github()
+{
+    local org=Streetless
+    local team=EnVRonment
+    local team_id=10498894
 
-# Get figma token
+    echo "Adding $epitech_email to $org/$team"
 
-# Get lucidchart token
+    gh api /orgs/$org/invitations -f "email=$epitech_email" -f "role=direct_member" -F "team_ids[]=$team_id"
+}
 
-# Get gitea token
+function add_user_to_youtrack()
+{
+    if [ -z "$youtrack_token" ]; then
+        echo -n "Enter the youtrack token: "
+        read youtrack_token
+    fi
+}
 
-# Get coolify token
 
-username=$(echo $email | cut -d'@' -f1) # Username for pld, youtrack
+function add_user_to_mailcow()
+{
+    if [ -z "$mailcow_token" ]; then
+        echo -n "Enter the mailcow token: "
+        read mailcow_token
+    fi
+}
 
-# Add user to our github organization
+function add_user_to_infisical()
+{
+    if [ -z "$infisical_token" ]; then
+        echo -n "Enter the infisical token: "
+        read infisical_token
+    fi
+}
 
-org=Streetless
-team=EnVRonment
-team_id=10498894
+function add_user_to_figma()
+{
+    if [ -z "$figma_token" ]; then
+        echo -n "Enter the figma token: "
+        read figma_token
+    fi
+}
 
-echo "Adding $email to $org/$team"
+function add_user_to_lucidchart()
+{
+    if [ -z "$lucidchart_token" ]; then
+        echo -n "Enter the lucidchart token: "
+        read lucidchart_token
+    fi
+}
 
-gh api /orgs/$org/invitations -f "email=$email" -f "role=direct_member" -F "team_ids[]=$team_id"
+function add_user_to_gitea()
+{
+    if [ -z "$gitea_token" ]; then
+        echo -n "Enter the gitea token: "
+        read gitea_token
+    fi
+}
 
-# Create a new user in our pld
+function add_user_to_coolify()
+{
+    if [ -z "$coolify_token" ]; then
+        echo -n "Enter the coolify token: "
+        read coolify_token
+    fi
+}
 
+function add_user_to_jsreport()
+{
+    # Get jsreport admin account
+    if [ -z "$jsreport_username" ]; then
+        echo -n "Enter the jsreport admin username: "
+        read jsreport_username
+    fi
+
+    if [ -z "$jsreport_password" ]; then
+        echo -n "Enter the jsreport admin password: "
+        read -s jsreport_password
+    fi
+
+    jsreport_token=$(echo -n "$jsreport_username:$jsreport_password" | base64)
+}
+
+function main()
+{
+    local functions=(
+        add_user_to_github
+        add_user_to_youtrack
+        add_user_to_mailcow
+        add_user_to_infisical
+        add_user_to_figma
+        add_user_to_lucidchart
+        add_user_to_gitea
+        add_user_to_coolify
+        add_user_to_jsreport
+    )
+
+    functionChoosen="$1"
+
+    if [ -n "$functionChoosen" ]; then
+        $functionChoosen
+        return
+    fi
+
+    for function in "${functions[@]}"; do
+        $function
+    done
+}
+
+main $@
